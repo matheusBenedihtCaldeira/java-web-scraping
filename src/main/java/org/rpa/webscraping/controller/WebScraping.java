c5ac2216d9a33d6a05fbce3870c5b5f304835f26
bene <matheusbenediht@gmail.com>
2023-12-13 16:27:00 -0300

    Update: Capturando produtos

--- src/main/java/org/rpa/webscraping/controller/WebScraping.java
+++ src/main/java/org/rpa/webscraping/controller/WebScraping.java
@@ -1,12 +1,15 @@
 package org.rpa.webscraping.controller;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
 import java.util.Collections;
+import java.util.List;
 
 public class WebScraping {
     public static void main(String[] args) {
@@ -14,7 +17,7 @@ public class WebScraping {
     }
 
     private static void extractData() {
-        //web driver
+        //Web driver config
         System.setProperty("webdriver.chrome.driver", "src/main/resources/msedgedriver.exe");
         EdgeOptions options = new EdgeOptions();
 
@@ -23,8 +26,26 @@ public class WebScraping {
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", null);
-
         WebDriver driver = new EdgeDriver(options = options);
+
+        //access the page
         driver.get("https://www.kabum.com.br/");
+
+        //Input for search
+        WebElement input = driver.findElement(By.xpath("//input[@id=\"input-busca\"]"));
+        input.sendKeys("placa de video 4060");
+        input.submit();
+        delay(5000);
+
+        List<WebElement> productsDescriptions = driver.findElements(By.xpath("//span[@class=\"sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard\"]"));
+        System.out.println(productsDescriptions);
+    }
+
+    private static void delay(long delay) {
+        try {
+            new Thread().sleep(delay);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
